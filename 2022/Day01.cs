a3957af9a50fafd3760b55a800f16d363a9a6d11
Niklas Wendel <niklas.wendel@gmail.com>
2024-12-08 17:35:49 +0100

    2023-01

--- 2022/Day01.cs
+++ 2022/Day01.cs
@@ -3,25 +3,18 @@
 public class Day01 : AdventBase
 {
     protected override object InternalPart1()
-    {
-        var max = Input.Blocks
+        => Input.Blocks
             .Select(x => x.Lines
                 .Select(int.Parse)
                 .Sum())
             .Max();
 
-        return max;
-    }
-
     protected override object InternalPart2()
-    {
-        var sum3 = Input.Blocks
+        => Input.Blocks
             .Select(x => x.Lines
                 .Select(int.Parse)
                 .Sum())
             .OrderByDescending(x => x)
             .Take(3)
             .Sum();
-        return sum3;
-    }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿var solutions = new AdventSolutions();
-var day = solutions.GetDay(2022, 1);
+var day = solutions.GetDay(2023, 1);
 
 await day.DownloadInputAsync();
 
