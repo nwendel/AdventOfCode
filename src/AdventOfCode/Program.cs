30957e9c117238596d015862dd942921709e0085
Niklas Wendel <niklas.wendel@gmail.com>
2025-12-07 07:14:30 +0100

    cleanup

--- src/AdventOfCode/Program.cs
+++ src/AdventOfCode/Program.cs
@@ -3,5 +3,5 @@ var solutions = new Solutions();
 var day = new Day(2025, 7);
 await solutions.Solve(day);
 
-//await solutions.Verify(day);
+await solutions.Verify(day);
 //await solutions.VerifyAll();
